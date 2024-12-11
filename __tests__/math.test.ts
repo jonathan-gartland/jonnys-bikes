5a8b4414c2b17b6be55e61ab04e6e7130d10a621
jonathan-gartland <jonathan-gartland@gmail.com>
2024-12-11 09:38:45 -0500

    commit this stuff

--- __tests__/math.test.ts
+++ __tests__/math.test.ts
@@ -8,4 +8,10 @@ describe("Sum function", () => {
     sum1 = sum(2, 3);
     expect(sum1).toEqual(5);
   });
+
+  test("Returns correct value", () => {
+    let sum1: number = 0;
+    sum1 = sum(5, 3);
+    expect(sum1).toEqual(8);
+  });
 });
