c088c16029d992746a1bc387dbf8b79c5a3053c5
jonathan-gartland <jonathan-gartland@gmail.com>
2024-12-10 21:35:06 -0500

    chore: fix asset path

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig = {
    *
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
-  basePath: "/mpn-lookup-nextjs",
+  basePath: "/jonnys-bikes",
   /**
    * Disable server-based image optimization. Next.js does not support
    * dynamic features with static exports.
