d107eaa65cdf8857f751cde83d1a7a971c9fe6ca
jonathan-gartland <jonathan-gartland@gmail.com>
2024-12-10 22:47:58 -0500

    commit this stuff

--- __tests__/home.test.tsx
+++ __tests__/home.test.tsx
@@ -2,9 +2,15 @@ import { render, screen } from '@testing-library/react';
 import Home from '../src/pages/index';
 
 describe('Home component', () => {
+
     test('renders the string "Save and see your changes instantly"', () => {
         render(<Home />);
         const textElement = screen.getByText(/Save and see your changes instantly/i);
         expect(textElement).toBeInTheDocument();
     });
+
+    test('it passes', () => {
+        expect(1).toBe(2-1);
+    });
+
 });
