87a265124488bd5b95d3cbaddce98a411740076d
jonathan-gartland <jonathan-gartland@gmail.com>
2024-12-10 23:22:47 -0500

    commit this stuff

---
+++ __tests__/math.test.ts
@@ -0,0 +1,11 @@
+import { sum } from "@/mathadd";
+
+import { describe, test, expect } from "@jest/globals";
+
+describe("Sum function", () => {
+  test("Returns correct value", () => {
+    let sum1: number = 0;
+    sum1 = sum(2, 3);
+    expect(sum1).toEqual(5);
+  });
+});
