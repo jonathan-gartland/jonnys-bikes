81ef5de7f26b2ef7906fb24cd47219de30cc4fec
jonathan-gartland <jonathan-gartland@gmail.com>
2024-12-15 19:44:02 -0500

    test: add initial e2e test, verify loading about component

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,29 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test.describe("e2e tests for Jonny's Bikes SPA", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/jonnys-bikes');
+  });
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+  test.afterEach(async ({ page }) => {
+    await page.close();
+  });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  test("Verify on Jonny's Bikes Page", async ({ page }) => {
+    await expect(page.getByText(/Jonny's Bikes/)).toBeVisible();
+  });
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  test('Verify delayed Loading of About section via text change', async ({
+    page,
+  }) => {
+    const aboutHeaderText =
+      /Delayed Loading component, and a brief description of the infographic SPA/;
+    const loadingText = /Loading.../;
+    const elementLoading = page.getByText(loadingText);
+    // override builtin 5s waits to test 3 sec delay load
+    await expect(elementLoading).toHaveText(loadingText, { timeout: 1 });
+    await page.waitForTimeout(3000); // confirmed this test fails if wait
+    // set to less than 3s, which is expected
+    await expect(elementLoading).toHaveText(aboutHeaderText, { timeout: 1 });
+  });
 });
